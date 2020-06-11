572b59d2bc651fc3d66393b45c1573b3af82bfee
franjlorenzo <63590328+franjlorenzo@users.noreply.github.com>
2020-06-11 00:11:18 -0300

    Curriculum con MVC

---
+++ CV_MVC/Ejercicio1_CV/Controllers/CurriculumController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio1_CV.Controllers
+{
+    public class CurriculumController : Controller
+    {
+        // GET: Curriculum
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CV()
+        {
+            return View();
+        }
+    }
+}
---
+++ CV_MVC/Ejercicio1_CV/Models/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace Ejercicio1_CV.Models
+{
+    public class Persona
+    {
+        [Display(Name = "Jack")]
+        public string nombre { get => nombre; set => nombre = value; }
+        [Display(Name = "Daniels")]
+        public string apellido { get => apellido; set => apellido = value; }
+        [Display(Name = "Puesto: Programador Senior")]
+        public string puesto { get => puesto; set => puesto = value; }
+        [Display(Name = "35156005")]
+        public long telefono { get => telefono; set => telefono = value; }
+        [Display(Name = "SI")]
+        public bool estadoCivil { get => estadoCivil; set => estadoCivil = value; }
+
+        public Persona(string nombre, string apellido, string puesto, long telefono, bool estadoCivil)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.puesto = puesto;
+            this.telefono = telefono;
+            this.estadoCivil = estadoCivil;
+        }
+
+        
+
+        
+       
+
+
+    }
+}
